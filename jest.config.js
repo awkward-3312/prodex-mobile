d533153d7b2802c390920763fdc69ea4fdd1b001
awkward-3312 <betza_es77@hotmail.com>
2026-09-11 08:37:35 -0600

    chore: align Expo SDK 57 dependencies

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   preset: 'jest-expo',
+  moduleDirectories: ['node_modules', '<rootDir>/node_modules/expo/node_modules'],
   testMatch: ['<rootDir>/__tests__/**/*.test.ts'],
 };
